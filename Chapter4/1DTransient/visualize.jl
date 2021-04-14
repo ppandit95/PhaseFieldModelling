d16cc827594c588357ed44b0a9d881c6eddb558e
Pushkar A Pandit <pushkarpandit95@gmail.com>
2021-04-14 15:33:00 +0530

    Completed first draft of the code

--- Chapter4/1DTransient/visualize.jl
+++ Chapter4/1DTransient/visualize.jl
@@ -1,6 +1,7 @@
 using DelimitedFiles,Plots;pyplot()
-data = "Initial_Profile.dat"
+data = ["Initial_Profile.dat" "Output-0.dat" "Output-1.dat" "Output-2.dat" "Output-3.dat" "Output-4.dat" "Output-5.dat" "Output-6.dat"]
 dfp = @__DIR__
 data = string(dfp,"/",data)
 D = readdlm(data)
 plot(D)
+savefig(string(chop(data,tail=3),"png"))
