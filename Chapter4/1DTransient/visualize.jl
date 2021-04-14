ea38da26a6677b6a63737cd77c576f591330f9f0
Pushkar A Pandit <pushkarpandit95@gmail.com>
2021-04-14 15:48:26 +0530

    Modified visualize.jl to give image output of each .dat file

--- Chapter4/1DTransient/visualize.jl
+++ Chapter4/1DTransient/visualize.jl
@@ -1,7 +1,9 @@
 using DelimitedFiles,Plots;pyplot()
-data = ["Initial_Profile.dat" "Output-0.dat" "Output-1.dat" "Output-2.dat" "Output-3.dat" "Output-4.dat" "Output-5.dat" "Output-6.dat"]
+data = ["Initial_Profile.dat" "Output-0.dat" "Output-100.dat" "Output-200.dat" "Output-300.dat" "Output-400.dat" "Output-500.dat" "Output-600.dat" "Output-700.dat" "Output-800.dat"]
 dfp = @__DIR__
-data = string(dfp,"/",data)
-D = readdlm(data)
-plot(D)
-savefig(string(chop(data,tail=3),"png"))
+for i in 1:10
+    data[i] = string(dfp,"/",data[i])
+    D = readdlm(data[i])
+    plot(D)
+    savefig(string(chop(data[i],tail=3),"png"))
+end
