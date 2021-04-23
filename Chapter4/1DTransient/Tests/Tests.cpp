aef877cdb6be8682c82c00f08d756f2efbb84b31
Pushkar A Pandit <pushkarpandit95@gmail.com>
2021-04-23 15:10:34 +0530

    Working test with inbuilt boost testing librarygit add Makefile Tests/

---
+++ Chapter4/1DTransient/Tests/Tests.cpp
@@ -0,0 +1,16 @@
+/*
+ * Tests.cpp
+ *
+ *  Created on: 23-Apr-2021
+ *      Author: pushkar
+ */
+
+#define BOOST_TEST_MODULE sample
+#include "boost/test/included/unit_test.hpp"
+
+BOOST_AUTO_TEST_CASE(TESTa){
+
+}
+
+
+
