63cc6ef24c8161ef5472f4805135aa3448724d9e
Pushkar A Pandit <pushkarpandit95@gmail.com>
2021-04-23 15:40:16 +0530

    Working testing framework but the program is designed so specific that it donot give the opportunity to write tests

--- Chapter4/1DTransient/Tests/Tests.cpp
+++ Chapter4/1DTransient/Tests/Tests.cpp
@@ -7,9 +7,17 @@
 
 #define BOOST_TEST_MODULE sample
 #include "boost/test/included/unit_test.hpp"
+#include "../src/transient.hpp"
+#include"../src/structures.hpp"
+TransientProblem	dummy;
+Params  p = dummy.GetParameters();
 
-BOOST_AUTO_TEST_CASE(TESTa){
-
+BOOST_AUTO_TEST_CASE(init_field){
+	dummy.initialize_field();
+	for(int i=0;i<20;i++){
+		BOOST_CHECK(p.T[(p.N/2) + i] == 1.0);
+		BOOST_CHECK(p.T[(p.N/2) - i] == 1.0);
+	}
 }
 
 
