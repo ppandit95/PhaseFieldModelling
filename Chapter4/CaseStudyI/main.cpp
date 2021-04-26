bc25792911e8a6787adfbb3c2d6a6aaaab5086a5
Pushkar A Pandit <pushkarpandit95@gmail.com>
2021-04-26 14:30:21 +0530

    Computed chemical potential at each point

--- Chapter4/CaseStudyI/main.cpp
+++ Chapter4/CaseStudyI/main.cpp
@@ -5,6 +5,7 @@
 #include<iostream>
 #include<time.h>
 #include<fstream>
+#include<cmath>
 int main(int argc, char **argv) {
 	//Get initial wall time
 	struct timespec begin,end;
@@ -18,7 +19,7 @@ int main(int argc, char **argv) {
 	int Ny = 64;
 	double dx = 1.0;
 	double dy = 1.0;
-	double** con,mu;
+	double** con,**mu,**dfdc;
 
 	//Time Integration Parameters
 	int nstep = 10000;
@@ -26,18 +27,22 @@ int main(int argc, char **argv) {
 	double dtime = 0.01;
 	double ttime = 0.0;
 
+
 	//Material Specific Parameters
 	double c0 = 0.4;
 	double mobility = 1.0;
 	double grad_coef = 0.5;
+	double A = 1.0;
 
 	//Initial concentration profile
 	srand( (unsigned)time( NULL ) );
 	con = new double*[Nx];
 	mu = new double* [Nx];
+	dfdc = new double* [Nx];
 	for(int i=0;i<Nx;i++){
 		con[i] = new double[Ny];
 		mu[i] = new double[Ny];
+		dfdc[i] = new double[Ny];
 	}
 	double noise = 0.02;
 	for(int i=0;i<Nx;i++){
@@ -50,8 +55,8 @@ int main(int argc, char **argv) {
 	std::ofstream initProf("Initial_Profile.dat");
 	for(int i=0;i<Nx;i++){
 		for(int j=0;j<Ny;j++)
-			initProf << con[i][j] << "\t";
-		initProf << std::endl;
+			initProf << i <<"\t"<<j<<"\t"<<con[i][j] << "\n";
+		//initProf << std::endl;
 	}
 
 	//Evolve with Cahn Hilliard Formulation
@@ -59,17 +64,30 @@ int main(int argc, char **argv) {
 		for(int i=0;i<Nx;i++){
 			for(int j=0;j<Ny;j++){
 				//Compute the chemical potential at each time step and each point
+				mu[i][j] = A*(2.0*con[i][j]*con[i][j]*std::pow((1-con[i][j]),2) + 2.0*std::pow(con[i][j],2)*(1-con[i][j]));
+
+
 
 			}
 		}
-
-
+		//Write the Chemical Potential in a dat file at specific intervels
+		if(istep%1000 == 0){
+			std::string filename = "Chemical_Potential_at_t="+std::to_string(istep)+".dat";
+			std::ofstream MuProf(filename);
+			for(int i=0;i<Nx;i++){
+				for(int j=0;j<Ny;j++)
+					MuProf << i <<"\t"<<j<<"\t"<<con[i][j] << "\n";
+			}
+			MuProf.close();
+		}
 
 	}
 	for(int i=0;i<Nx;i++){
 		delete[] con[i];
 		delete[] mu[i];
+		delete[] dfdc[i];
 	}
+	delete[] dfdc;
 	delete[] mu;
 	delete[] con;
 	return 0;
