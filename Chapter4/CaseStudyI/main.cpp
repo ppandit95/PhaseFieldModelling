a06036603a35cdc3267e004b1282ad9b3ac9424a
Pushkar A Pandit <pushkarpandit95@gmail.com>
2021-04-26 15:03:59 +0530

    Computed functional derivative at each point

--- Chapter4/CaseStudyI/main.cpp
+++ Chapter4/CaseStudyI/main.cpp
@@ -19,7 +19,7 @@ int main(int argc, char **argv) {
 	int Ny = 64;
 	double dx = 1.0;
 	double dy = 1.0;
-	double** con,**mu,**dfdc;
+	double** con,**mu,**dfdc,**laplace_con;
 
 	//Time Integration Parameters
 	int nstep = 10000;
@@ -39,10 +39,12 @@ int main(int argc, char **argv) {
 	con = new double*[Nx];
 	mu = new double* [Nx];
 	dfdc = new double* [Nx];
+	laplace_con = new double* [Nx];
 	for(int i=0;i<Nx;i++){
 		con[i] = new double[Ny];
 		mu[i] = new double[Ny];
 		dfdc[i] = new double[Ny];
+		laplace_con[i] = new double[Ny];
 	}
 	double noise = 0.02;
 	for(int i=0;i<Nx;i++){
@@ -66,13 +68,42 @@ int main(int argc, char **argv) {
 				//Compute the chemical potential at each time step and each point
 				mu[i][j] = A*(2.0*con[i][j]*con[i][j]*std::pow((1-con[i][j]),2) + 2.0*std::pow(con[i][j],2)*(1-con[i][j]));
 
+				//Compute laplacian of concentration in case of periodic BCs
+				if(i==0&&j==0)
+					laplace_con[i][j] = std::pow(1/dx,2)*(con[i+1][j]-2*con[i][j]+con[i+Nx-1][j])
+										+ std::pow(1/dy,2)*(con[i][j+1]-2*con[i][j]+con[i][j+Ny-1]);
+				else if(i==(Nx-1)&&j==0)
+					laplace_con[i][j] = std::pow(1/dx,2)*(con[i+1-Nx][j]-2*con[i][j]+con[i-1][j])
+										+ std::pow(1/dy,2)*(con[i][j+1]-2*con[i][j]+con[i][j+Ny-1]);
+				else if(i==(Nx-1)&&j==(Ny-1))
+					laplace_con[i][j] = std::pow(1/dx,2)*(con[i+1-Nx][j]-2*con[i][j]+con[i-1][j])
+										+ std::pow(1/dy,2)*(con[i][j+1-Ny]-2*con[i][j]+con[i][j-1]);
+				else if(i==0&&j==(Ny-1))
+					laplace_con[i][j] = std::pow(1/dx,2)*(con[i+1][j]-2*con[i][j]+con[i-1+Nx][j])
+															+ std::pow(1/dy,2)*(con[i][j+1-Ny]-2*con[i][j]+con[i][j-1]);
+				else if(i==0)
+					laplace_con[i][j] = std::pow(1/dx,2)*(con[i+1][j]-2*con[i][j]+con[i+Nx-1][j]) +
+										std::pow(1/dy,2)*(con[i][j+1]-2*con[i][j]+con[i][j-1]);
+				else if(i==(Nx-1))
+					laplace_con[i][j] = std::pow(1/dx,2)*(con[i+1-Nx][j]-2*con[i][j]+con[i-1][j]) +
+										std::pow(1/dy,2)*(con[i][j+1]-2*con[i][j]+con[i][j-1]);
+				else if(j==0)
+					laplace_con[i][j] = std::pow(1/dx,2)*(con[i+1][j]-2*con[i][j]+con[i-1][j]) +
+										std::pow(1/dy,2)*(con[i][j+1]-2*con[i][j]+con[i][j-1+Ny]);
+				else if(j==(Ny-1))
+					laplace_con[i][j] = std::pow(1/dx,2)*(con[i+1][j]-2*con[i][j]+con[i-1][j]) +
+										std::pow(1/dy,2)*(con[i][j+1-Ny]-2*con[i][j]+con[i][j-1]);
+				else
+					laplace_con[i][j] = std::pow(1/dx,2)*(con[i+1][j]-2*con[i][j]+con[i-1][j]) +
+										std::pow(1/dy,2)*(con[i][j+1]-2*con[i][j]+con[i][j-1]);
 
-
+				//Compute functional derivative of free energy with concentration
+				dfdc[i][j] = mu[i][j] - grad_coef*laplace_con[i][j];
 			}
 		}
 		//Write the Chemical Potential in a dat file at specific intervels
 		if(istep%1000 == 0){
-			std::string filename = "Chemical_Potential_at_t="+std::to_string(istep)+".dat";
+			std::string filename = "Chemical_Potential_at_t_"+std::to_string(istep)+".dat";
 			std::ofstream MuProf(filename);
 			for(int i=0;i<Nx;i++){
 				for(int j=0;j<Ny;j++)
@@ -80,13 +111,25 @@ int main(int argc, char **argv) {
 			}
 			MuProf.close();
 		}
+		//Write the Derivative in a dat file at specific intervels
+		if(istep%1000 == 0){
+		std::string filename = "Derivative_at_t_"+std::to_string(istep)+".dat";
+		std::ofstream DerProf(filename);
+		for(int i=0;i<Nx;i++){
+				for(int j=0;j<Ny;j++)
+					DerProf << i <<"\t"<<j<<"\t"<<con[i][j] << "\n";
+			}
+			DerProf.close();
+		}
 
 	}
 	for(int i=0;i<Nx;i++){
 		delete[] con[i];
 		delete[] mu[i];
 		delete[] dfdc[i];
+		delete[] laplace_con[i];
 	}
+	delete[] laplace_con;
 	delete[] dfdc;
 	delete[] mu;
 	delete[] con;
